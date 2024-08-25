f54242a05cebb53d101f782309f2827078c4d80c
Zehra232006 <mehdizehra31@gmail.com>
2024-08-25 19:37:24 +0530

    commit

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 let input = document.getElementById('inputBox');
 let buttons = document.querySelector('button');
+
+let string = "";
+let arr  = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListner('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+
+        string += e.target.innerHTML;
+        input.value = string;
+    })
+})
